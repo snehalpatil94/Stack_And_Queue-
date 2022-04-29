7bd0880672f12a5d5238df0c39789841acdbfd37
snehalpatil94 <erpatilsnehal@gmail.com>
2022-04-29 19:11:35 +0530

    Program to create Queue

---
+++ src/com/bridgelabz/stackandqueue/QueueMain.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.stackandqueue;
+
+import java.util.Scanner;
+
+/**
+ * Ability to create Queue
+ * 
+ * @author : Snehal Patil
+ * 
+ */
+public class QueueMain {
+	public static void main(String[] args) {
+		System.out.println("------- Welcome to Stacks and Queues Program ---------");
+		System.out.println();
+
+		QueueImplementation queueImplementation = new QueueImplementation();
+
+		// Calling operation menu method of QueueImplementation
+		queueImplementation.operationsMenu();
+	}
+}
